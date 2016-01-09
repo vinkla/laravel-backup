c4b30f18936200077cf081ff4196e2cc5976cdf4
Vincent Klaiber <vincentklaiber@gmail.com>
2016-01-09 16:25:02 +0100

    Test name is string

--- tests/FactoryTrait.php
+++ tests/FactoryTrait.php
@@ -25,6 +25,11 @@ trait FactoryTrait
         $this->assertInstanceOf($this->getInterface(), $this->getFactory());
     }
 
+    public function testNameIsString()
+    {
+        $this->assertTrue(is_string($this->getFactory()->getName()));
+    }
+
     protected function getInterface()
     {
         return FactoryInterface::class;
